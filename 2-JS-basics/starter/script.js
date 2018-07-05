d98cd16dfd9eb8fa54011a566179055f6b12f344
Andrzej Knitter <andrzej.knitter@gmail.com>
2018-07-05 09:16:19 +0200

    Basic operators - lesson 10

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -23,7 +23,7 @@ var if = 23;
 /*****************************
 * Variable mutation and type coercion
 */
-
+/*
 var firstName = 'John';
 var age = 28;
 // Type coercion
@@ -38,5 +38,39 @@ job = 'driver';
 alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+*/
+
+/***
+ Basic operators
+ */
+
+ var year, yearJohn, yearMark;
+ now = 2018;
+ ageJohn = 28;
+ ageMark = 33;
+
+ //Math operators
+ yearJohn = now-28;
+ yearMark = now - 33;
+
+ console.log(yearJohn);
+
+ console.log(now + 2);
+ console.log(now * 2);
+ console.log(now / 2);
+
+ // Logical operators
+
+ var johnOlder = ageJohn > ageMark;
+ console.log(johnOlder);
+
+ // typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'zgfkshgdkfsd');
+var x;
+console.log(typeof x);
+
+
 
 
