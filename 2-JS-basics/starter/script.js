493bceda70537106736335e72337a6ec748f417b
Andrzej Knitter <andrzej.knitter@gmail.com>
2018-07-06 12:57:42 +0200

    Objects- lesson 25

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -385,6 +385,7 @@ switch(true) {
  * Challenge 3
  */
 //
+/*
  function tipCalculator(bill) {
      var tip;
      if (bill < 50) {
@@ -408,4 +409,34 @@ var finalValues = [bills[0] + tips[0],
                     bills[1] + tips[1],
                     bills[2] + tips[2]];
 console.log(tips, finalValues);
+*/
 
+/**************
+ * Objects and properties
+ */
+
+ // Object literal
+ var john = {
+     firstName: 'John',
+     lastName: 'Smith',
+     birthYear: 1990,
+     family: ['Jane', 'Mark', 'Bob', 'Emily'],
+     job: 'teacher',
+     isMarried: false
+ };
+
+ console.log(john.firstName);
+ console.log(john['lastName']);
+ var x = 'birthYear';
+ console.log(john[x]);
+
+ john.job = 'designer';
+ john['isMarried'] = true;
+ console.log(john);
+
+ // New Object syntax
+ var jane = new Object();
+ jane.name = 'Jane';
+ jane.birthYear = 1968;
+ jane['lastName'] = 'Smith';
+ console.log(jane);
