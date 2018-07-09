a4754cb3b20073a998312eb2d64dad3d6415a2b0
Andrzej Knitter <andrzej.knitter@gmail.com>
2018-07-09 12:46:42 +0200

    Loops and iteractions - lesson 29

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -467,7 +467,7 @@ console.log(john);
 /**************
  * Challenge 4
  */
-
+/*
 var mark = {
     fullName: 'Mark Smith',
     mass: 92,
@@ -497,13 +497,44 @@ if (john.calcBMI() > mark.calcBMI()) {
 } else if (john.calcBMI() === mark.calcBMI()) {
     console.log(john.fullName + ' and ' + mark.fullName + ' have the same BMI.');
 }
-
-
-/*
-
-
-var isBmiHigher = bmiMark >= bmiJohn;
-
-console.log(bmiMark, bmiJohn);
-console.log('Is Mark\'s BMI higher than John\'s?' + ' ' + isBmiHigher);
 */
+
+/**************
+ * Loops and iteration
+ */
+/*
+ for (var i = 0; i < 10; i++) {
+     console.log(i);
+ }
+
+ var john = ['John', 'Smith', 1990, 'teacher', false];
+
+ for (var i = 0; i < john.length; i++) {
+     console.log(john[i]);
+ }
+
+ // while loop
+ var i = 0;
+ while(i < john.length) {
+    console.log(john[i]);
+    i++;
+ }
+*/
+ //continue and break statements
+ var john = ['John', 'Smith', 1990, 'teacher', false];
+
+ for (var i = 0; i < john.length; i++) {
+     if (typeof john[i] !== 'string') continue;
+     console.log(john[i]);
+ }
+
+ for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// looping backwards
+
+for (var i = john.length - 1; i >= 0; i--)  {
+    console.log(john[i]);
+}
