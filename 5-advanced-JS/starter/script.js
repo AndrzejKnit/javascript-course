d80b84411a0bb1086138b334092b5078e584705b
Andrzej Knitter <andrzej.knitter@gmail.com>
2018-07-12 16:30:44 +0200

    Functions returning functions - lesson 66

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -95,7 +95,7 @@ console.log(obj.city);
 */
 
 // Passing functions as arguments
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -130,3 +130,34 @@ var rates = arrayCalc(ages,maxHeartRate);
 console.log(ages);
 //console.log(fullAges);
 console.log(rates);
+*/
+
+
+// Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+      return function(name) {
+          console.log(name + ', can you please what UX design is?');
+      }
+  }  else if (job == 'teacher') {
+      return function(name) {
+        console.log('What subject do you teach, ' + name + '?');
+      }
+  } else {
+      return function(name) {
+          console.log('Hello ' + name + ', what do you do?');
+      }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
+
+var designerQuestion = interviewQuestion('designer');
+designerQuestion('Jane');
+
+interviewQuestion('teacher')('Mark'); 
+
+
+
