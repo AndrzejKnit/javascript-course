aaa7b12c7485bca51521eb40fad65c1d9e453342
Andrzej Knitter <andrzej.knitter@gmail.com>
2018-07-19 11:49:55 +0200

    Closures - Lecture 68

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -169,6 +169,7 @@ function game()  {
 
 game();
 */
+/*
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -180,6 +181,57 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5);
 })(5);
+*/
 
+//////////////////////////
+//Closures
 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge- age) + a);
+    }
+}
 
+var retirementUS = retirement(66);
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please what UX design is?');
+        }
+    }  else if (job == 'teacher') {
+        return function(name) {
+          console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+  }
+  */
+
+ function interviewQuestion(job) {
+     return function(name) {
+        if (job === 'designer') {
+                console.log(name + ', can you please what UX design is?');
+
+        }  else if (job == 'teacher') {
+              console.log('What subject do you teach, ' + name + '?');
+        } else {
+                console.log('Hello ' + name + ', what do you do?');
+        }
+     }
+ }
+
+ interviewQuestion('teacher')('John');
