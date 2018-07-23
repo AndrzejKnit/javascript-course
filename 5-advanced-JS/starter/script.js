5b771ad30116bac1921053b0c130791f21ebb412
AndrzejKnit <andrzej.knitter@gmail.com>
2018-07-23 15:23:25 +0200

    coding challenge 7 part 2 - lesson 72

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -332,7 +332,7 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
-
+/*
 (function() {
 
     function Question(question, answer, correct) {
@@ -381,17 +381,7 @@ var answer = parseInt(prompt('Please select the correct answer.'));
 questions[n].checkAnswer(answer);
 })();
 
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 /*
@@ -405,3 +395,81 @@ questions[n].checkAnswer(answer);
 
 11. Display the score in the console. Use yet another method for this.
 */
+
+(function() {
+
+    function Question(question, answer, correct) {
+    this.question = question;
+    this.answer = answer;
+    this.correct = correct;
+}
+Question.prototype.displayQuestion =
+   function() {
+       
+    console.log(this.question);
+
+    for (var i = 0; i < this.answer.length; i++) {
+        console.log(i + ': ' +
+        this.answer[i]);
+    }
+}
+
+Question.prototype.checkAnswer =
+function(ans, callback) {
+    var sc;
+    if (ans === this.correct) {
+        console.log('Correct answer.');
+        sc = callback(true);
+    } else {
+        console.log('Wrong answer');
+        sc = callback(false);
+    }
+    this.displayScore(sc);
+}
+
+Question.prototype.displayScore = function(score) {
+    console.log('your current score is: ' + score);
+    console.log('---------------------------');
+}
+
+var q1 = new Question('Is JavaScrept the coolest programming language in the worls?',
+['Yes', 'No'],
+0);
+var q2 = new Question('What is the namo of this soure\'s teacher?',
+['John', 'Michael','Jonas'],
+2);
+var q3 = new Question('What does best describe coding?',
+['Boring', 'Hard', 'Fun','Tedious'],
+2);
+
+var questions = [q1, q2, q3];
+
+function score() {
+    var sc = 0;
+    return function(correct) {
+        if (correct) {
+            sc++;
+        }
+        return sc;
+    }
+}
+
+var keepScore = score();
+
+function nextQuestion() {
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = prompt('Please select the correct answer.');
+if (answer !== 'exit') {
+    questions[n].checkAnswer(parseInt(answer), keepScore);
+    nextQuestion();
+}
+
+}
+
+nextQuestion();
+})();
+
