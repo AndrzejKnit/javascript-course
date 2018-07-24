22af68f91d310ec09ef61cf9bc70a2ee1bfb8c5f
AndrzejKnit <andrzej.knitter@gmail.com>
2018-07-24 14:51:56 +0200

    Reading input data - lesson 79

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,29 +1,42 @@
-var budgetControler = (function() {
-    var x=23;
 
-    var add = function(a) {
-        return x + a;
-    }
- return {
-     publicTest: function(b) {
-         return add(b);
-     }  
- }
+// Budget controller
+var budgetControler = (function() {
+
+    // some code
+
 })();
 
+
+ // UI controller
 var UIControler = (function() {
 
     // Some code
 
 })();
 
+// Global app controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z =  budgetCtrl.publicTest(5);
-  return {
-      anotherPublic: function() {
-          console.log(z);
-      }
-  }
+    var ctrlAddItem = function() {
+        console.log('test');
+        // 1. Get the filed input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display he budget on the UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+    })
 
 })(budgetControler, UIControler);
