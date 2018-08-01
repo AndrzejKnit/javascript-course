5569bb2da0141013719e88a7a55234a57a958c63
AndrzejKnit <andrzej.knitter@gmail.com>
2018-08-01 12:17:06 +0200

    Updating the budget: UI Controler - lesson 87

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -102,7 +102,11 @@ var UIControler = (function() {
         inputValue: '.add__value',
         inputBtb: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
     return {
         getInput: function() {
@@ -145,6 +149,18 @@ var UIControler = (function() {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+            document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp;
+
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMStrings.percentageLabel).textContent = '---';
+            }
+        },
 
         getDOMstrings: function() {   //metoda umozliwia dostęp publiczny do DOMStrings
             return DOMStrings;
@@ -177,7 +193,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
     // 3. Display he budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
@@ -204,6 +220,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: 0
+            });
             setupEventListners();
         }
     };
