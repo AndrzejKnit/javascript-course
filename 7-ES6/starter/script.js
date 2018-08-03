58e039be730c0aa740111762eeedc52c3ccf41a7
AndrzejKnit <andrzej.knitter@gmail.com>
2018-08-03 13:37:10 +0200

    Arrow functions basic - lesson 107

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -74,7 +74,7 @@ console.log(c);
 
 ///////////////
 // Strings
-
+/*
 let firstName = 'John';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -96,3 +96,31 @@ console.log(n.startsWith('j'));
 console.log(n.endsWith('th'));
 console.log(n.includes('m'));
 console.log(`${firstName} `.repeat(5));
+*/
+
+///////////////////////////
+/// Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+//ES5
+var ages5 = years.map(function(el) {
+    return 2016 - el;
+});
+console.log(ages5);
+
+// ES6
+
+let ages6 = years.map(el => 2016 - el);
+
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
