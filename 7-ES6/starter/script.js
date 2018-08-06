24b5cd197bd6a6126b0b7ca8430dc43f6b4cd804
AndrzejKnit <andrzej.knitter@gmail.com>
2018-08-06 16:32:58 +0200

    Destructuring: Lesson 109

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -175,6 +175,7 @@ var friends = ['Bob', 'Jane', 'Mark'];
 new Person('John').myFriends5(friends);
 */
 //ES6
+/*
 Person.prototype.myFriends6 = function(friends) {
 
     var arr = friends.map((el) => `${this.name} is friends with ${el}`);
@@ -183,3 +184,44 @@ Person.prototype.myFriends6 = function(friends) {
 }
 var friends = ['Bob', 'Jane', 'Mark'];
 new Person('John').myFriends6(friends);
+*/
+
+
+//////////////////
+// Destructuring
+/*
+// ES5
+var john = ['John', 26];
+//var name = john[0];
+var age = john[1];
+
+// ES6
+const [name, year] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+     firstName: 'John',
+     lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+function calcAgeRetirement(year) {
+
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
+*/
+
