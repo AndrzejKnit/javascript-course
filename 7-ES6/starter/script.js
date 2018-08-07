e0095b12de740e4cc616eeffdb1619530b778fb6
AndrzejKnit <andrzej.knitter@gmail.com>
2018-08-07 14:10:23 +0200

    rest parameters: lesson 112

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -282,7 +282,7 @@ console.log(ages.find(cur => cur >=18));
 
 ////////////////////
 // Spread operator
-
+/*
 function addFourAges (a, b, c, d) {
     return a + b + c + d;
 }
@@ -306,5 +306,55 @@ console.log(bigFamily);
 
 const h = document.querySelector('h1');
 const boxes = document.querySelectorAll('.box');
-const all = [h, ...boxes];
+const all = [h, ...boxes];                           // spread '...' jest używany w function call
 Array.from(all).forEach(cur => cur.style.color = 'purple');
+*/
+
+/////////////
+/// Rest operator
+/*
+//ES5
+function isFullAge5() {
+    //console.log(arguments);
+    var argArr = Array.prototype.slice.call(arguments);
+
+    argArr.forEach(function(cur) {
+        console.log((2016 - cur) >= 18);
+    });
+
+}
+
+isFullAge5(1990, 1999, 1965);
+isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+//ES6
+function isFullAge6(...years) {                                 //rest '...' jest uzywany w deklaracji funkcji
+    years.forEach(cur => console.log((2016 - cur) >= 18));
+
+}
+
+isFullAge6(1990, 1999, 1965);
+*/
+
+//ES5
+function isFullAge5(limit) {
+    console.log(arguments);
+    var argArr = Array.prototype.slice.call(arguments,1);
+    console.log(argArr);
+
+    argArr.forEach(function(cur) {
+        console.log((2016 - cur) >= limit);
+    });
+
+}
+
+//isFullAge5(21, 1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+//ES6
+function isFullAge6(limit, ...years) {                                 //rest '...' jest uzywany w deklaracji funkcji
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+
+}
+
+isFullAge6(18, 1990, 1999, 1965);
