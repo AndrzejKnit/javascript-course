6a4917771aa561bcd9c03c0b39834fd9edc21aab
AndrzejKnit <andrzej.knitter@gmail.com>
2018-10-19 16:42:33 +0200

    Lesson 139

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,24 +1,9 @@
-import axios from 'axios';
+import Search from './models/Search';
+const search = new Search('pizza');
 
-async function getResults(query) {
+console.log(search);
+search.getResults();
 
-    const key ='87c57a1c06692338e5221fa0128cff7b';
-
-    try {
-   const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-   const recipes = res.data.recipes;
-   console.log(recipes);
- } catch (error) {
-     alert(error);
- }
-}
-
-getResults('pizza');
-
-
-//https://www.food2fork.com/api/get
-//https://www.food2fork.com/api/search
-//
 
 
 
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an ecport string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+
+        const key ='87c57a1c06692338e5221fa0128cff7b';
+    
+        try {
+       const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+       this.result = res.data.recipes;
+      // console.log(this.result);
+     } catch (error) {
+         alert(error);
+     }
+    }
+}
