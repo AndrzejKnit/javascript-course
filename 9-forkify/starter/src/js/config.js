2316e59091c4ccb8446f6b2dddaacc8315b748af
AndrzejKnit <andrzej.knitter@gmail.com>
2018-10-31 15:48:19 +0100

    Lesson 145

---
+++ 9-forkify/starter/src/js/config.js
@@ -0,0 +1,2 @@
+export const key ='87c57a1c06692338e5221fa0128cff7b';
+
--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /**Global state of the app
@@ -9,6 +10,9 @@ import { elements, renderLoader, clearLoader } from './views/base';
  * */
 const state = {};
 
+/*
+SEARCH CONTROLER
+*/
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput();
@@ -43,7 +47,15 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.clearResults();
         searchView.renderResults(state.search.result, goToPage);
     }
-})
+});
+
+/*
+RECIPE CONTROLER
+*/
+
+const r = new Recipe(46956);
+r.getRecipe();
+console.log(r);
 
 
 
